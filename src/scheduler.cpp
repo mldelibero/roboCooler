d0ccf1c93f09958cf617b6907b565ac83bad86d3
Michael <mldelibero@gmail.com>
2015-10-10 15:01:59 -0400

    Sched is working at 1kHz Heartbeat is working at 1Hz Test toggle is working.

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -7,18 +7,27 @@
 #include "stm32f4xx.h"
 #include "stm32f407xx.h"
 
-#define SCHED_TIMx_IRQn               TIM1_UP_TIM10_IRQn
-#define HB_CNT_RESET                  (SCHED_CLK / HB_FREQ)
-#define HB_FREQ                       2
-#define HB_GPIOO_SPEED                GPIO_Low_Speed
-#define HB_GPIO_MODE                  GPIO_Mode_OUT
-#define HB_GPIO_PIN_X                 GPIO_Pin_12
-#define HB_GPIOx                      GPIOD
-#define SCHED_RCC_APBxPeriphClockCmd  RCC_APB2PeriphClockCmd
 #define RCC_APBxPeriph_SCHED_RCC      RCC_APB2Periph_TIM1
-#define SCHED_CLK                     10000
-#define SCHED_TIM_CLK                 1000
+#define SCHED_RCC_APBxPeriphClockCmd  RCC_APB2PeriphClockCmd
+#define SCHED_TIMx_IRQn               TIM1_UP_TIM10_IRQn
 #define SCHED_TIMx                    TIM1
+#define SCHED_CLK                     10000
+#define SCHED_FREQ                    1000
+#define SCHED_PER                     (SCHED_CLK / SCHED_FREQ - 1)
+
+#define HB_AHB1Periph_GPIOx           RCC_AHB1Periph_GPIOD
+#define HB_GPIOx                      GPIOD
+#define HB_GPIO_PIN_X                 GPIO_Pin_12
+#define HB_GPIO_MODE                  GPIO_Mode_OUT
+#define HB_GPIO_SPEED                 GPIO_Low_Speed
+#define HB_FREQ                       2
+#define HB_CNT_RESET                  (SCHED_FREQ / HB_FREQ)
+
+#define TEST_AHB1Periph_GPIOx         RCC_AHB1Periph_GPIOC
+#define TEST_GPIOx                    GPIOC
+#define TEST_GPIO_PIN_X               GPIO_Pin_7
+#define TEST_GPIO_MODE                GPIO_Mode_OUT
+#define TEST_GPIO_SPEED               GPIO_High_Speed
 
 uint16_t PrescalerValue;
 
@@ -26,16 +35,31 @@ void init_heartBeatLed(void)
 {
     GPIO_InitTypeDef GPIO_InitStruct;
 
+    RCC_AHB1PeriphClockCmd(HB_AHB1Periph_GPIOx, ENABLE);
+
     GPIO_DeInit(HB_GPIOx);
     GPIO_StructInit(&GPIO_InitStruct);
 
     GPIO_InitStruct.GPIO_Pin   = HB_GPIO_PIN_X;
     GPIO_InitStruct.GPIO_Mode  = HB_GPIO_MODE;
-    GPIO_InitStruct.GPIO_Speed = HB_GPIOO_SPEED;
+    GPIO_InitStruct.GPIO_Speed = HB_GPIO_SPEED;
     GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
     GPIO_InitStruct.GPIO_PuPd  = GPIO_PuPd_NOPULL;
 
     GPIO_Init(HB_GPIOx,&GPIO_InitStruct);
+
+    RCC_AHB1PeriphClockCmd(TEST_AHB1Periph_GPIOx, ENABLE);
+
+    GPIO_DeInit(TEST_GPIOx);
+    GPIO_StructInit(&GPIO_InitStruct);
+
+    GPIO_InitStruct.GPIO_Pin   = TEST_GPIO_PIN_X;
+    GPIO_InitStruct.GPIO_Mode  = TEST_GPIO_MODE;
+    GPIO_InitStruct.GPIO_Speed = TEST_GPIO_SPEED;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+
+    GPIO_Init(TEST_GPIOx,&GPIO_InitStruct);
 }
 
 void init_scheduler(void)
@@ -46,11 +70,11 @@ void init_scheduler(void)
     TIM_DeInit(SCHED_TIMx);
     SCHED_RCC_APBxPeriphClockCmd(RCC_APBxPeriph_SCHED_RCC, ENABLE);
 
-    PrescalerValue = (uint16_t) ((SystemCoreClock / 2) / SCHED_CLK) - 1;
+    PrescalerValue = (uint16_t) ((SystemCoreClock) / SCHED_CLK) - 1;
 
     TIM_TimeBaseInitStruct.TIM_Prescaler         = PrescalerValue;
     TIM_TimeBaseInitStruct.TIM_CounterMode       = TIM_CounterMode_Up;
-    TIM_TimeBaseInitStruct.TIM_Period            = SCHED_CLK / SCHED_TIM_CLK;
+    TIM_TimeBaseInitStruct.TIM_Period            = SCHED_PER;
     TIM_TimeBaseInitStruct.TIM_ClockDivision     = TIM_CKD_DIV1;
 
     TIM_TimeBaseInit(SCHED_TIMx, &TIM_TimeBaseInitStruct);
@@ -73,8 +97,10 @@ void SCHED_IRQHandler(void)
 {
     static int32_t HB_Cnt = HB_CNT_RESET;
 
+
     if (--HB_Cnt <= 0)
     {
+      GPIO_ToggleBits(TEST_GPIOx, TEST_GPIO_PIN_X);
       GPIO_ToggleBits(HB_GPIOx, HB_GPIO_PIN_X);
       HB_Cnt = HB_CNT_RESET;
     }
