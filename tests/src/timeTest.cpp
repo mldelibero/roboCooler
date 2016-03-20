f2c303c86213537e0434520a4b14c9b74d629dca
Michael <mldelibero@gmail.com>
2016-03-20 16:47:42 -0400

    started a test for time

---
+++ tests/src/timeTest.cpp
@@ -0,0 +1,20 @@
+#include <CppUTest/TestHarness.h>
+
+#include "time.h"
+
+TEST_GROUP(TimeTests)
+{
+	void setup()
+	{
+	}
+	void teardown()
+	{
+	}
+};
+
+TEST(TimeTests, Fail)
+{
+	CHECK(false);
+}
+
+
