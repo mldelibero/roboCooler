f4460e554ba08942d14d6f3a13b0dc841f7e7582
Delibero.Michael <delibero.michael@ensco.com>
2016-04-30 20:15:23 -0400

    removed motor command from pushbutton -- encapsulation

--- code/src/pushButton.cpp
+++ code/src/pushButton.cpp
@@ -20,8 +20,3 @@ void Run_PushButtons(void)
 	IsTimerExpired(timer);
 }
 
-motorCmd_t Get_MotorCmd(void)
-{
-	return MOTOR_STOP;
-} // end - motorCmd_t Get_MotorCmd(void)
-
--- code/tests/allTests/pushButtonTest.cpp
+++ code/tests/allTests/pushButtonTest.cpp
@@ -6,11 +6,11 @@
 
 /*  * List of features needed:
 
-    * Initialize gpio input
-    * Create filter for debouncing
-    * Create Integration test for input
-    * Create user for system
-*/
+ * Initialize gpio input
+ * Create filter for debouncing
+ * Create Integration test for input
+ * Create user for system
+ */
 
 TEST_GROUP(PushButtonInitTests)
 {
@@ -30,33 +30,28 @@ TEST(PushButtonInitTests, TestInitCalls)
 
     mock().expectOneCall("RCC_AHB1PeriphClockCmd" ).withCallOrder(callOrder++);
     mock().expectOneCall("RCC_AHB1PeriphClockCmd" ).withCallOrder(callOrder++);
-    mock().expectOneCall("GPIO_StructInit"		  ).withCallOrder(callOrder++);
-    mock().expectOneCall("GPIO_Init"			  ).withCallOrder(callOrder++);
-    mock().expectOneCall("GPIO_Init"			  ).withCallOrder(callOrder++);
+    mock().expectOneCall("GPIO_StructInit"        ).withCallOrder(callOrder++);
+    mock().expectOneCall("GPIO_Init"              ).withCallOrder(callOrder++);
+    mock().expectOneCall("GPIO_Init"              ).withCallOrder(callOrder++);
 
-    mock().expectOneCall("AllocateTimer"		  ).withCallOrder(callOrder++);
+    mock().expectOneCall("AllocateTimer"          ).withCallOrder(callOrder++);
 } // end - TEST(PushButtonInitTests, TestInitCalls)
 
 TEST_GROUP(PushButtonFilterTests)
 {
     void setup()
     {
-	}
+    }
     void teardown()
     {
         mock().checkExpectations();
         mock().clear();
-	}
+    }
 }; // end - TEST_GROUP(PushButtonFilterTests)
-
-TEST(PushButtonFilterTests, MotorCmdStopInitially)
-{
-	CHECK(Get_MotorCmd() == MOTOR_STOP);
-}
 
 TEST(PushButtonFilterTests, OnlySamplesButtonsOnTimeOut)
 {
-    mock().expectOneCall("IsTimerExpired"		  );
-	Run_PushButtons();
+    mock().expectOneCall("IsTimerExpired"         );
+    Run_PushButtons();
 }
 
