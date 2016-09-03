ff1368b8db4cd765cb8f0d8498cca7e237be9606
Delibero.Michael <delibero.michael@ensco.com>
2016-09-03 14:32:51 -0400

    timer.cpp -- fixed spacing

--- code/src/timer.cpp
+++ code/src/timer.cpp
@@ -1,11 +1,8 @@
 /* brief
-	The timer module will subtract one from an array of timers at a fixed interval inside of an interrupt. It will not lower a timer below zero. It will have no knowlede of the meaning of any of the timers. Resetting the timers will be the responsibility of the client.
-*/
-#include <stdint.h>
-#include <stdio.h>
+   The timer module will subtract one from an array of timers at a fixed interval inside of an interrupt. It will not lower a timer below zero. It will have no knowlede of the meaning of any of the timers. Resetting the timers will be the responsibility of the client.
+   */
 #include <stm32f4xx_tim.h>
 #include "timer.h"
-
 #include "timerDriver.h"
 
 static int32_t timers[NUM_TIMERS];
@@ -13,24 +10,24 @@ static uint32_t numTimersAllocated = 0;
 
 uint32_t Get_NumTimersAllocated(void)
 {
-	return numTimersAllocated;
+    return numTimersAllocated;
 }
 
 uint32_t Init_Timers(void)
 {
     init_TimerDriver();
-	numTimersAllocated = 0;
-	for (int i = 0; i < NUM_TIMERS; i++)
-	{
-		timers[i] = 0;
-	}
-	return numTimersAllocated;
+    numTimersAllocated = 0;
+    for (int i = 0; i < NUM_TIMERS; i++)
+    {
+        timers[i] = 0;
+    }
+    return numTimersAllocated;
 }
 
 int32_t AllocateTimer(void)
 {
-	if (numTimersAllocated < NUM_TIMERS) return (int32_t)numTimersAllocated++;
-	else 								 return -1;
+    if (numTimersAllocated < NUM_TIMERS) return (int32_t)numTimersAllocated++;
+    else                                 return -1;
 }
 
 bool IsTimerExpired(int32_t timer)
@@ -41,12 +38,12 @@ bool IsTimerExpired(int32_t timer)
 
 void  Set_TimerValue(int32_t timer, int32_t timerValue)
 {
-	if (timer >= 0 && timer < NUM_TIMERS) timers[timer] = timerValue;
+    if (timer >= 0 && timer < NUM_TIMERS) timers[timer] = timerValue;
 }
 
 int32_t Get_TimerValue(int32_t timer)
 {
-	return timers[timer];
+    return timers[timer];
 }
 
 void TIMER_IRQHandler(void)
