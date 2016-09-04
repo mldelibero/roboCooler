ff0d15d5982c1a2824ded9f4d4a472e38e260998
Delibero.Michael <delibero.michael@ensco.com>
2016-09-03 20:07:13 -0400

    added mock call to set led gpio values

--- code/tests/mockFiles/ledDriver.cpp
+++ code/tests/mockFiles/ledDriver.cpp
@@ -6,3 +6,8 @@ void init_ledDriver(void)
     mock().actualCall("init_ledDriver");
 }
 
+void SetLeds(uint8_t ledValues)
+{
+    mock().actualCall("SetLeds")
+    .withParameter("ledValues", ledValues);
+}
