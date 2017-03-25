3e31c693fde815a991797f23097873839fad146e
delibero.michael <delibero.michael@ensco.com>
2017-03-25 13:11:46 -0400

    loop tests now run

--- code/src/loop.cpp
+++ code/src/loop.cpp
@@ -1,16 +1,16 @@
 #include "loop.h"
 
-//#include "capTouch.h"
-//#include "limitSwitch.h"
-//#include "lidMotor.h"
+#include "capTouch.h"
+#include "limitSwitch.h"
+#include "lidMotor.h"
 #include "utils.h"
 #include "ledStrip.h"
 #include "ledStripDriver.h"
 
-//extern CLimSwComp Opened_Limit;
-//extern CLimSwComp Closed_Limit;
-//extern CCapTouchComp CapTouch;
-//extern CLidMotorComp LidMotor;
+extern CLimSwComp Opened_Limit;
+extern CLimSwComp Closed_Limit;
+extern CCapTouchComp CapTouch;
+extern CLidMotorComp LidMotor;
 extern CLedStripDriver LedStripDriver;
 extern CLedStripComp LedStrip;
 
@@ -18,10 +18,10 @@ void loop(void)
 {
     WHILE(1)
     {
-//        Opened_Limit.Run();
-//        Closed_Limit.Run();
-//        CapTouch.Run();
-//        LidMotor.Run();
+        Opened_Limit.Run();
+        Closed_Limit.Run();
+        CapTouch.Run();
+        LidMotor.Run();
         LedStrip.Run();
         LedStripDriver.Run();
     }
